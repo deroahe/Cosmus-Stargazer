39591f123d0f66f8b9aa28a517e5df4f98436b90
deroahe <deroahe@users.noreply.github.com>
2020-09-21 15:51:42 +0300

    Added a bit of css

--- app.py
+++ app.py
@@ -1,20 +1,12 @@
 from flask import Flask, render_template, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 import datetime
+from BlogPost import BlogPost
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///posts.db'
 db = SQLAlchemy(app)
 
-class BlogPost(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(100), nullable=False)
-    content = db.Column(db.Text, nullable=False)
-    author = db.Column(db.String(20), nullable=False, default="N/A")
-    date_posted = db.Column(db.String(100), nullable=False, default=datetime.datetime.now().strftime("%c"))
-
-    def __repr__(self):
-        return 'Blog post ' + str(self.id)
 
 @app.route('/')
 def index():
